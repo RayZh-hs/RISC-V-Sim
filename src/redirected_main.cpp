8d793c4938a370211ab91ee479eeeda698c0a35e
RayZh <rayzhangshanghai@hotmail.com>
2025-07-31 11:55:08 +0800

    chore: add a redirected main entrypoint for debugging convenience

---
+++ src/redirected_main.cpp
@@ -0,0 +1,25 @@
+// redirected_main.cpp
+// - for debugging purposes: redirect input from a file
+
+#include "third_party/logger.hpp"
+#include "control.hpp"
+#include "utility/constants.hpp"
+
+namespace C = norb::riscv::constants;
+
+int main(int argc, char *argv[]) {
+    std::freopen("../testcases/array_test2.data", "r", stdin);
+    auto &logger = Logger::get();
+
+    // Configure logger based on constants
+    if (not C::log_file.empty()) {
+        logger.setFileOutput(C::log_file);
+    }
+    logger.setLevel(C::log_level);
+
+    norb::riscv::RISCV_Simulator simulator;
+    simulator.boot();
+    simulator.run();
+
+    return 0;
+}
